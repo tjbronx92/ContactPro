bdb1d5f34b4107abcad25859987716e29771ef90
tjbronx92 <39333780+tjbronx92@users.noreply.github.com>
2023-12-24 14:12:16 -0800

    App Migration

---
+++ ContactPro/Models/Contact.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+using ContactPro.Enums;
+
+namespace ContactPro.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required]
+        
+        public string? AppUserId { get; set; }
+        
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = " The {0} must be at leat {2} and a max of {1} chracters long.", MinimumLength = 2)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = " The {0} must be at leat {2} and a max of {1} long.", MinimumLength = 2)]
+        public string? LastName { get; set; }
+
+        [NotMapped]
+        public string? FullName { get { return $"{FirstName} {LastName}"; } }
+
+        [Display(Name = "Birthday")]
+        [DataType(DataType.Date)] 
+        public DateTime? BirthDate { get; set; }
+
+        [Required]
+        public string? Address1 { get; set; }
+        public string? Address2 { get; set; }
+
+        [Required]
+        public string? City { get; set; }
+
+        [Required]
+        public States State { get; set; }
+
+        [Required]
+        [Display(Name = "Zip Code")]
+        [DataType(DataType.PostalCode)]
+        public int ZipCode { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Numeber")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime? Created { get; set; }
+
+        //Image Properties
+        public byte[]? ImageData { get; set; }
+        public string? ImageType { get; set; }
+
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
+
+        //Virtuals
+        public virtual AppUser? AppUser { get; set; }
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+    }
+}
